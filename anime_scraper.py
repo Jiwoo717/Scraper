651fba40cb6822591d12aaf05942b3c5ed4a14a9
Eric <89174461+Jiwoo717@users.noreply.github.com>
2023-08-28 13:56:23 -0700

    Update anime_scraper.py
    Yagmail feature to send emails to my address

--- anime_scraper.py
+++ anime_scraper.py
@@ -1,18 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import yagmail
 
-driver = webdriver.Chrome(executable_path='C:\Users\kimer\Desktop\Programming\Drivers\Chrome Driver\chromedriver-win64')
+
+# Setting up Chrome driver options
+driver_path = 'C:/Users/kimer/Desktop/Programming/Drivers/Chrome Driver/chromedriver-win64'
+options = Options()
+options.binary_location = driver_path
+
+# Initialize Chrome driver
+driver = webdriver.Chrome(options=options)
 
 driver.get('https://www.animenewsnetwork.com/')
 
-headlines = driver.find_elements_by_class_name('wrap')
+headlines = driver.find_elements(By.CLASS_NAME, 'wrap')
 headlines_text = [headline.text for headline in headlines]
 
-driver.close()
+df = pd.DataFrame(headlines_text, columns=['Headline'])
+df = df[~df['Headline'].str.contains("Keyword_to_exclude")]
 
-yag = yagmail.SMTP('kimeric717@email.com', 'My_Password')
-
+yag = yagmail.SMTP('YourEmail@gmail.com', 'Password')
 yag.send(
     to='kimeric717@email.com',
     subject='Daily Headlines',
